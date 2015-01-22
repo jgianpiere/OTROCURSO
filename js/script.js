deaddf3e838935aa63805a0593538b03d38abf72
Gianpiere Julio Ramos Bernuy <jgianpiere@gmail.com>
2015-01-22 10:12:28 -0500

    initial commit
    Empezando el proyecto

---
+++ js/script.js
@@ -0,0 +1,155 @@
+(function() {
+
+	/* PAGINA OBJETIVO */
+
+	var objetivo_1 = $('.objetivo p');
+
+	TweenLite.from(objetivo_1, 1.5, {scaleX:0, scaleY:0});
+
+	var objetivo_2 = $('.objetivo h2');
+
+	TweenLite.from(objetivo_2, 1.5, {scaleX:0, scaleY:0});
+
+	var slide_line = $('hr');
+
+	TweenLite.from(slide_line, 1.5, {scaleX:0, scaleY:0});
+
+	/* PAGINA1-1 */
+
+	var slide_alert1_1 = $('#slide-pag1-1 .pag-center img');
+
+	TweenLite.from(slide_alert1_1, 1.5, {scaleX: 0, scaleY: 0});
+
+	var slide_text1_1 = $('#slide-pag1-1 .fold-objetivo .pag-center .text-pag1-1');
+
+	TweenLite.from(slide_text1_1, 1.5, {scaleX: 0, scaleY: 0});
+
+	/* PAGINA1-3 */
+
+	var slide_alert1_3 = $('#slide-pag1-3 .pag-center img');
+
+	TweenLite.from(slide_alert1_3, 1.5, {scaleX: 0, scaleY: 0});
+
+	var slide_text1_3 = $('#slide-pag1-3 .text-pag1-1');
+
+	TweenLite.from(slide_text1_3, 1.5, {scaleX: 0, scaleY: 0});
+
+	/* PAGINA1-4 */
+
+	var slide_circle_left = $('.circle-left');
+
+	TweenLite.from(slide_circle_left, 1.5, {opacity: 0});
+
+	var slide_circle_right = $('.circle-right');
+
+	TweenLite.from(slide_circle_right, 4.5, {opacity: 0, delay: 1});
+
+	/* PAGINA1-5 */
+
+	var slide_circle_a = $('.pag1_5a');
+
+	TweenLite.from(slide_circle_a, 1.5, {opacity: 0});
+
+	var slide_circle_b = $('.pag1_5b');
+
+	TweenLite.from(slide_circle_b, 1.5, {opacity: 0, delay: 1});
+
+	var slide_circle_c = $('.pag1_5c');
+
+	TweenLite.from(slide_circle_c, 1.5, {opacity: 0, delay: 2});
+
+	/* PAGINA1-6 */
+
+	var slide_alert = $('.fondo-next');
+
+	TweenLite.from(slide_alert, 1.5, {opacity: 0});
+
+	var slide_alert_text = $('.fondo-next2');
+
+	TweenLite.from(slide_alert_text, 1.5, {opacity: 0, delay: 1});
+
+	/* PAGINA2-2 */
+
+	var slide_alert2_2 = $('#slide-pag2-2 .pag-center img');
+
+	TweenLite.from(slide_alert2_2, 1.5, {scaleX: 0, scaleY: 0});
+
+	var slide_text2_2 = $('#slide-pag2-2 .text-pag1-1');
+
+	TweenLite.from(slide_text2_2, 1.5, {scaleX: 0, scaleY: 0});
+
+	/* PAGINA2-5 */
+
+	var slide_cuadro_a = $('.circle-cuadro-a');
+
+	TweenLite.from(slide_cuadro_a, 1, {opacity: 0});
+
+	var slide_cuadro_b = $('.circle-cuadro-b');
+
+	TweenLite.from(slide_cuadro_b, 2, {opacity: 0, delay: 1});
+
+	var slide_cuadro_c = $('.circle-cuadro-c');
+
+	TweenLite.from(slide_cuadro_c, 3, {opacity: 0, delay: 2});
+
+	var slide_cuadro_d = $('.circle-cuadro-d');
+
+	TweenLite.from(slide_cuadro_d, 4, {opacity: 0, delay: 3});
+
+	var slide_cuadro_e = $('.circle-cuadro-e');
+
+	TweenLite.from(slide_cuadro_e, 5, {opacity: 0, delay: 4});
+
+	var slide_cuadro_f = $('.circle-cuadro-f');
+
+	TweenLite.from(slide_cuadro_f, 6, {opacity: 0, delay: 5});
+
+	var slide_cuadro_g = $('.circle-cuadro-g');
+
+	TweenLite.from(slide_cuadro_g, 7, {opacity: 0, delay: 6});
+
+	/* PAGINA2-6 */
+
+	var slide_cuadro_h = $('.circle-cuadro-h');
+
+	TweenLite.from(slide_cuadro_h, 1, {opacity: 0});
+
+	var slide_cuadro_i = $('.circle-cuadro-i');
+
+	TweenLite.from(slide_cuadro_i, 2, {opacity: 0, delay: 1});
+
+	var slide_cuadro_j = $('.circle-cuadro-j');
+
+	TweenLite.from(slide_cuadro_j, 3, {opacity: 0, delay: 2});
+
+	var slide_cuadro_k = $('.circle-cuadro-k');
+
+	TweenLite.from(slide_cuadro_k, 4, {opacity: 0, delay: 3});
+
+	var slide_cuadro_l = $('.circle-cuadro-l');
+
+	TweenLite.from(slide_cuadro_l, 5, {opacity: 0, delay: 4});
+
+
+	/* PAGINA2-7 */
+
+	var slide_content_flecha = $('.peligro-expone-flecha');
+
+	TweenLite.from(slide_content_flecha, 1.5, {opacity: 0});
+
+	var slide_table = $('.table');
+
+	TweenLite.from(slide_table, 3.5, {opacity: 0, delay: 1});
+
+	var slide_table_a = $('#tabla-a');
+
+	TweenLite.from(slide_table_a, 5.5, {opacity: 0, delay: 3});
+
+	var slide_table_b = $('#tabla-b');
+
+	TweenLite.from(slide_table_b, 7.5, {opacity: 0, delay: 5});
+
+	var slide_table_c = $('#tabla-c');
+
+	TweenLite.from(slide_table_c, 9.5, {opacity: 0, delay: 7});
+})();
